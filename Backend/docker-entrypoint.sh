281ad41c4f16ebbe6e66420b2b09bae8d8c13ec8
AnkitCS4075 <68578561+AnkitCS4075@users.noreply.github.com>
2026-07-26 21:23:53 +0530

    update dockerfile

--- Backend/docker-entrypoint.sh
+++ Backend/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Running Prisma schema push..."
-npx prisma db push --schema=src/prisma/db/schema.prisma --accept-data-loss --skip-generate
+npx prisma db push --schema=src/prisma/db/schema.prisma --accept-data-loss
 
 echo "Starting server..."
 exec node src/server.js
